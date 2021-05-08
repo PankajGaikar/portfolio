526daf4f441f0b223ea11f8da8b67c59320b27e4
PANKYAAA <pankajs.gaikar@gmail.com>
2021-05-08 23:50:49 +0530

    Updated ui design.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
-// import 'package:example/exp.dart';
-// import './exp.dart';
 import 'package:flutter/material.dart';
+import 'package:portfolio/contants.dart' as Constants;
 import 'package:portfolio/flutter_particle_bg.dart';
-// import 'exp.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,16 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,7 +36,6 @@ class _MyHomePageState extends State<MyHomePage> {
         body: ParticleBackground(
           backgroundcolor: Colors.black,
           pointcolor: Colors.white.withOpacity(0.2),
-          // bgimg: AssetImage('assets/bg.png'),
           child: Stack(
             alignment: Alignment.topCenter,
             children: [
@@ -59,25 +45,46 @@ class _MyHomePageState extends State<MyHomePage> {
                   alignment: Alignment.center,
                   decoration: BoxDecoration(color: Colors.white),
                   height: MediaQuery.of(context).size.height * .75,
-                  width: MediaQuery.of(context).size.width * .6,
+                  width: MediaQuery.of(context).size.width * .4,
                   child: ListView(
                     children: [
                       Text(
-                        'PANKAJ GAIKAR',
+                        Constants.TITLE,
                         textAlign: TextAlign.center,
                         style: TextStyle(
                             fontSize: 40, fontWeight: FontWeight.bold),
                       ),
+                      Container(
+                        height: 10,
+                      ),
                       Text(
-                        'iOS Developer',
+                        Constants.SUBTITLE,
                         textAlign: TextAlign.center,
                         style: TextStyle(
                             fontSize: 20, fontWeight: FontWeight.w600),
                       ),
+                      Container(
+                        height: 5,
+                      ),
                       Text(
-                        'iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer iOS Developer',
+                        Constants.SEMI_SUBTITLE,
                         textAlign: TextAlign.center,
                         style: TextStyle(fontWeight: FontWeight.w100),
+                      ),
+                      Container(
+                        height: 25,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40, right: 40),
+                        child: Text(
+                          Constants.INTRO_MESSAGE,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, height: 1.5),
+                        ),
+                      ),
+                      Container(
+                        height: 20,
                       ),
                       Text(
                         '#swift #Objective-C #C #C++ #Flutter #Dart',
