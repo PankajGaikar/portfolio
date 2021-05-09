43b7641a17cdfbd0144094bedd125ffc41dd142b
PANKYAAA <pankajs.gaikar@gmail.com>
2021-05-09 22:54:01 +0530

    Created responsive container.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/SocialButtonType.dart';
 import 'package:portfolio/contants.dart' as Constants;
 import 'package:portfolio/flutter_particle_bg.dart';
+import 'package:responsive/flex_widget.dart';
+import 'package:responsive/responsive_row.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -44,100 +46,112 @@ class _MyHomePageState extends State<MyHomePage> {
             alignment: Alignment.topCenter,
             children: [
               Center(
-                child: Container(
-                  padding: EdgeInsets.only(top: 135),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(color: Colors.white),
-                  height: MediaQuery.of(context).size.height * .75,
-                  width: MediaQuery.of(context).size.width * .4,
-                  child: Wrap(
-                    direction: Axis.horizontal,
-                    children: [
-                      Center(
-                        child: Text(
-                          Constants.TITLE,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 40, fontWeight: FontWeight.bold),
+                child: ResponsiveRow(
+                  columnsCount: 4,
+                  children: <Widget>[
+                    FlexWidget(
+                      child: Container(
+                        padding: EdgeInsets.only(top: 120),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(color: Colors.white),
+                        height: MediaQuery.of(context).size.height * .78,
+                        // width: MediaQuery.of(context).size.width * .4,
+                        child: Wrap(
+                          direction: Axis.horizontal,
+                          children: [
+                            Center(
+                              child: Text(
+                                Constants.TITLE,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 40, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Container(
+                              height: 10,
+                            ),
+                            Center(
+                              child: Text(
+                                Constants.SUBTITLE,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                            Container(
+                              height: 5,
+                            ),
+                            Center(
+                              child: Text(
+                                Constants.SEMI_SUBTITLE,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontWeight: FontWeight.w100),
+                              ),
+                            ),
+                            Container(
+                              height: 15,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 40, right: 40),
+                              child: Text(
+                                Constants.INTRO_MESSAGE,
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, height: 1.5),
+                              ),
+                            ),
+                            Container(
+                              height: 15,
+                            ),
+                            Center(
+                              child: Text(
+                                '#swift #Objective-C #C #C++ #Flutter #Dart',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontWeight: FontWeight.w700),
+                              ),
+                            ),
+                            Container(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              height: 50,
+                              child: Center(
+                                child: ListView(
+                                  shrinkWrap: true,
+                                  padding: const EdgeInsets.all(5.0),
+                                  scrollDirection: Axis.horizontal,
+                                  children: [
+                                    SocialConnectButton(
+                                        button: SocialButtonType.mail),
+                                    SocialConnectButton(
+                                        button: SocialButtonType.github),
+                                    SocialConnectButton(
+                                        button: SocialButtonType.stackoverflow),
+                                    SocialConnectButton(
+                                        button: SocialButtonType.linkedin),
+                                    SocialConnectButton(
+                                        button: SocialButtonType.skype),
+                                    SocialConnectButton(
+                                        button: SocialButtonType.twitter),
+                                    SocialConnectButton(
+                                        button: SocialButtonType.instagram),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 10,
+                            ),
+                          ],
                         ),
                       ),
-                      Container(
-                        height: 10,
-                      ),
-                      Center(
-                        child: Text(
-                          Constants.SUBTITLE,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                      Container(
-                        height: 5,
-                      ),
-                      Center(
-                        child: Text(
-                          Constants.SEMI_SUBTITLE,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontWeight: FontWeight.w100),
-                        ),
-                      ),
-                      Container(
-                        height: 25,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 40, right: 40),
-                        child: Text(
-                          Constants.INTRO_MESSAGE,
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, height: 1.5),
-                        ),
-                      ),
-                      Container(
-                        height: 20,
-                      ),
-                      Center(
-                        child: Text(
-                          '#swift #Objective-C #C #C++ #Flutter #Dart',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontWeight: FontWeight.w700),
-                        ),
-                      ),
-                      Container(
-                        height: 10,
-                      ),
-                      SizedBox(
-                        height: 50,
-                        child: Center(
-                          child: ListView(
-                            shrinkWrap: true,
-                            padding: const EdgeInsets.all(5.0),
-                            scrollDirection: Axis.horizontal,
-                            children: [
-                              SocialConnectButton(
-                                  button: SocialButtonType.mail),
-                              SocialConnectButton(
-                                  button: SocialButtonType.github),
-                              SocialConnectButton(
-                                  button: SocialButtonType.stackoverflow),
-                              SocialConnectButton(
-                                  button: SocialButtonType.linkedin),
-                              SocialConnectButton(
-                                  button: SocialButtonType.skype),
-                              SocialConnectButton(
-                                  button: SocialButtonType.twitter),
-                              SocialConnectButton(
-                                  button: SocialButtonType.instagram),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 10,
-                      ),
-                    ],
-                  ),
+                      xs: 3,
+                      sm: 3,
+                      md: 3,
+                      lg: 3,
+                    )
+                  ],
                 ),
               ),
               Container(
