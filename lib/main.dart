7d20baf8fb33d224858660639f711410dbb804b7
PANKYAAA <pankajs.gaikar@gmail.com>
2021-05-11 23:31:39 +0530

    Added scroll capability. Added placeholder view to adjust image.

--- lib/main.dart
+++ lib/main.dart
@@ -42,130 +42,147 @@ class _MyHomePageState extends State<MyHomePage> {
         body: ParticleBackground(
           backgroundcolor: Colors.black,
           pointcolor: Colors.white.withOpacity(0.2),
-          child: Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              Center(
-                child: ResponsiveRow(
-                  columnsCount: 12,
-                  children: <Widget>[
-                    FlexWidget(
-                      child: Container(
-                        padding: EdgeInsets.only(top: 120),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: Wrap(
-                          direction: Axis.horizontal,
-                          children: [
-                            Center(
-                              child: Text(
-                                Constants.TITLE,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 40, fontWeight: FontWeight.bold),
+          child: SingleChildScrollView(
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 100,
+                    ),
+                    Center(
+                      child: ResponsiveRow(
+                        columnsCount: 12,
+                        children: <Widget>[
+                          FlexWidget(
+                            child: Container(
+                              padding: EdgeInsets.only(top: 120),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(color: Colors.white),
+                              child: Wrap(
+                                direction: Axis.horizontal,
+                                children: [
+                                  Center(
+                                    child: Text(
+                                      Constants.TITLE,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 32,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 10,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      Constants.SUBTITLE,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 5,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      Constants.SEMI_SUBTITLE,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w100),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 15,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 40, right: 40),
+                                    child: Text(
+                                      Constants.INTRO_MESSAGE,
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          height: 1.5),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 15,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      '#swift #Objective-C #C #C++ #Flutter #Dart',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 10,
+                                  ),
+                                  SizedBox(
+                                    height: 50,
+                                    child: Center(
+                                      child: ListView(
+                                        shrinkWrap: true,
+                                        padding: const EdgeInsets.all(5.0),
+                                        scrollDirection: Axis.horizontal,
+                                        children: [
+                                          SocialConnectButton(
+                                              button: SocialButtonType.mail),
+                                          SocialConnectButton(
+                                              button: SocialButtonType.github),
+                                          SocialConnectButton(
+                                              button: SocialButtonType
+                                                  .stackoverflow),
+                                          SocialConnectButton(
+                                              button:
+                                                  SocialButtonType.linkedin),
+                                          SocialConnectButton(
+                                              button: SocialButtonType.skype),
+                                          SocialConnectButton(
+                                              button: SocialButtonType.twitter),
+                                          SocialConnectButton(
+                                              button:
+                                                  SocialButtonType.instagram),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 10,
+                                  ),
+                                ],
                               ),
                             ),
-                            Container(
-                              height: 10,
-                            ),
-                            Center(
-                              child: Text(
-                                Constants.SUBTITLE,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                            Container(
-                              height: 5,
-                            ),
-                            Center(
-                              child: Text(
-                                Constants.SEMI_SUBTITLE,
-                                textAlign: TextAlign.center,
-                                style: TextStyle(fontWeight: FontWeight.w100),
-                              ),
-                            ),
-                            Container(
-                              height: 15,
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(left: 40, right: 40),
-                              child: Text(
-                                Constants.INTRO_MESSAGE,
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500, height: 1.5),
-                              ),
-                            ),
-                            Container(
-                              height: 15,
-                            ),
-                            Center(
-                              child: Text(
-                                '#swift #Objective-C #C #C++ #Flutter #Dart',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(fontWeight: FontWeight.w700),
-                              ),
-                            ),
-                            Container(
-                              height: 10,
-                            ),
-                            SizedBox(
-                              height: 50,
-                              child: Center(
-                                child: ListView(
-                                  shrinkWrap: true,
-                                  padding: const EdgeInsets.all(5.0),
-                                  scrollDirection: Axis.horizontal,
-                                  children: [
-                                    SocialConnectButton(
-                                        button: SocialButtonType.mail),
-                                    SocialConnectButton(
-                                        button: SocialButtonType.github),
-                                    SocialConnectButton(
-                                        button: SocialButtonType.stackoverflow),
-                                    SocialConnectButton(
-                                        button: SocialButtonType.linkedin),
-                                    SocialConnectButton(
-                                        button: SocialButtonType.skype),
-                                    SocialConnectButton(
-                                        button: SocialButtonType.twitter),
-                                    SocialConnectButton(
-                                        button: SocialButtonType.instagram),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Container(
-                              height: 10,
-                            ),
-                          ],
-                        ),
+                            xs: 11,
+                            sm: 10,
+                            md: 10,
+                            lg: 10,
+                            xl: 10,
+                            xxl: 10,
+                            xxxl: 10,
+                          )
+                        ],
                       ),
-                      xs: 11,
-                      sm: 10,
-                      md: 10,
-                      lg: 10,
-                      xl: 10,
-                      xxl: 10,
-                      xxxl: 10,
                     )
                   ],
                 ),
-              ),
-              Container(
-                child: ClipOval(
-                  // radius: 100,
-                  child: Image.asset(
-                    'assets/Profile.png',
-                    height: 200,
-                    width: 200,
+                Container(
+                  child: ClipOval(
+                    // radius: 100,
+                    child: Image.asset(
+                      'assets/Profile.png',
+                      height: 200,
+                      width: 200,
+                    ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ));
   }
