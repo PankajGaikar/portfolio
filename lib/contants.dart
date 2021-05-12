55e65abd0dc67b91dde1b66a2d00b1889ff10389
PANKYAAA <pankajs.gaikar@gmail.com>
2021-05-12 09:11:28 +0530

    ☕️

--- lib/contants.dart
+++ lib/contants.dart
@@ -1,5 +1,5 @@
 const String TITLE = "Hey, I'm Pankaj 👋";
 const String SUBTITLE = "iOS Developer based in Oslo, Norway";
-const String SEMI_SUBTITLE = "Swift & Objective-C. Powered by 🍺️";
+const String SEMI_SUBTITLE = "Swift & Objective-C. Powered by ☕️️";
 const String INTRO_MESSAGE =
     "Hey There, I'm a iOS Developer from Pune in India. I love to design/implement good crazy things. I am currently working for TIDAL in Oslo, Norway. Apart from iOS, I also have experience working with Flutter (this website is 100% flutter👻) and Android with Java, Kotlin. \nWhen I am not working/programming/scratching my head, I usually play Fifa on PlayStation or watch Football ⚽️. I am a huge fan of FC Barcelona. \nI am always open to have talk about anything and look forward to connect with you";
